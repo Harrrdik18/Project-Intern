357e02abd36f0b279e2d7a823ddbcaf6c0ec17db
hardik <harrrdik18@gmail.com>
2024-06-09 00:16:44 +0530

    Complete Desktop Version

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,16 @@ function App() {
         <img className='logo' src={logo} alt='Logo' />
         <h2>Suite Of Business Support Services</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed</p>
+        <EmailAuthorization />
       </div>
       <div className='Second'>
-        <Card className="grid-item" Topic={`Presentation Design`} Image={`Analytics`}/>
-        <Card className="grid-item" Topic={`Audio - Visual Production`} Image={`Audio_visual`} />
-        <Card className="grid-item" Topic={`Translation Services`} Image={`Translate`} />
-        <Card className="grid-item" Topic={`Graphic Design`} Image={`Graphic`} />
-        <Card className="grid-item" Topic={`Research & Analytics`} Image={`Research`} />
-        <Card className="grid-item" Topic={`Data Processing`} Image={`Data-processing`} />
-      </div>
-      <div className='Third'>
-        <EmailAuthorization />
+      <Card className="grid-item" Topic={`Presentation Design`} Image={`Analytics`}/>
+      <Card className="grid-item" Topic={`Audio - Visual Production`} Image={`Audio_visual`} />
+      <Card className="grid-item" Topic={`Translation Services`} Image={`Translate`} />
+      <Card className="grid-item" Topic={`Graphic Design`} Image={`Graphic`} />
+      <Card className="grid-item" Topic={`Research & Analytics`}  Image={`Research`} />
+      <Card className="grid-item" Topic={`Data Processing`} Image={`Data-processing`} />
+        
       </div>
     </div>
   )
