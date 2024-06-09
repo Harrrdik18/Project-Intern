3a6a617a0479eba2c0ac259a776ba9c4cf400910
hardik <harrrdik18@gmail.com>
2024-06-09 12:30:41 +0530

    Fixed Images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ function Card({Topic, Image}) {
   return (
     <div className='card'>
       <div className='upper'>
-      <img src={`src/assets/${Image}.png`}></img>
+      <img src={`./${Image}.png`}></img>
       <h3>{Topic}</h3>
       </div>     
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed</p>
